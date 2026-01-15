e409c427551e772f4e238575d8d684f654a6ae5a
Akash shukla <87383979+729akashshukla@users.noreply.github.com>
2026-01-15 18:40:30 +0530

    Update vite.config.js to set base path for GitHub Pages deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        secure:false,
-        
-      },
+    base: "/Packer-mover/",
+    // Other configuration options...
+    server: {
+        // Server options...
     },
-  },
+    // other configurations...
 });
